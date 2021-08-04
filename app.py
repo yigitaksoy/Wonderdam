4cbf3d6a91f035a187ecdca9cb09f69a4d522537
yigitaksoy <yigitaksoy@gmail.com>
2021-08-04 13:33:19 +0200

    Add flask mail functionality for the contact form

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from flask_mail import Mail, Message
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
@@ -16,6 +17,24 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
+# -- Flask Mail Settings -- # 
+mail_settings = {
+    "MAIL_SERVER": os.environ.get('MAIL_SERVER'),
+    "MAIL_PORT": os.environ.get('MAIL_PORT'),
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": os.environ.get('MAIL_USE_SSL'),
+    "MAIL_USERNAME": os.environ.get('MAIL_USERNAME'),
+    "MAIL_PASSWORD": os.environ.get('MAIL_PASSWORD'),
+    "SECURITY_EMAIL_SENDER": os.environ.get("SECURITY_EMAIL_SENDER")
+}
+
+
+# -- Update application settings
+app.config.update(mail_settings)
+
+# -- Create instance of Flask Mail
+mail =Mail(app)
 
 mongo = PyMongo(app)
 
@@ -336,8 +355,21 @@ def delete_user(user_id):
 
 
 # -- Contact --- #
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        with app.app_context():
+            msg = Message(subject="New Email From Wonderdam Blog")
+            msg.recipients = [os.environ.get("MAIL_USERNAME")]
+            msg.sender = request.form.get("emailaddress")
+            name = request.form.get("name")
+            message = request.form.get("message")
+            msg.body = (f"Name: {name} \n"
+                        f"Email: {msg.sender} \n"
+                        f"Message: {message} \n")
+            mail.send(msg)
+            flash("Your Message Sent Successfully!")
+            return redirect(url_for("contact"))
 
     return render_template("contact.html")
 

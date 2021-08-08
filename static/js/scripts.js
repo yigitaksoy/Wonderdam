e0d3466e60c3507502a2630691837f8c98fe17d9
yigitaksoy <yigitaksoy@gmail.com>
2021-08-08 12:27:32 +0200

    Add function to show preview img on forms

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -36,3 +36,17 @@ function countChars(obj){
   
   document.getElementById("counter").innerHTML = strLength+' out of '+maxLength+' characters';
   }
+
+// Show input image preview on Add/Edit Post forms
+$('input[type="file"]').change(function(e) {
+  var fileName = e.target.files[0].name;
+  $("#file").val(fileName);
+
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    // get loaded data and render thumbnail.
+    document.getElementById("preview").src = e.target.result;
+  };
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+});

5c73311a804d436b8714f5661bb7832ac4e9aab1
yigitaksoy <yigitaksoy@gmail.com>
2021-08-11 02:35:50 +0200

    Remove title method from author data for edit post

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ def edit_post(post_id):
                     "post_image": upload_result["url"],
                     "post_address": request.form.get("post_address"),
                     "post_date": datetime.today().strftime("%d %B, %Y"),
-                    "author": session["user"].title()
+                    "author": session["user"]
                 }
                 mongo.db.posts.update({"_id": ObjectId(post_id)}, submit)
                 flash("Post Successfully Updated")
